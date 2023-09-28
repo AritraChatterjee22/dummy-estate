5dcb3b5377d7f112739cb6d4b607cdd0f0290604
Aritra <chatterjee.aritra@protonmail.com>
2023-09-28 11:35:11 +0530

    test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+
+
 
 dotenv.config()
 
@@ -16,3 +19,5 @@ const app = express();
 app.listen(3000, ()=>{
   console.log('Server is listening on port 3000')
 })
+
+app.use("/api/user", userRouter)
