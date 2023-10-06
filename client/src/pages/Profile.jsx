73a625560e18d7f33fe78cb10c326564a55e7d5e
Aritra <chatterjee.aritra@protonmail.com>
2023-10-07 00:23:27 +0530

    complete delete listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import {app} from '../firebase.js'
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from '../redux/user/userSlice.js'
 import {useDispatch} from 'react-redux'
 import {Link} from 'react-router-dom'
+import Listing from '../../../api/models/listing.model.js'
 
 
 export default function Profile() {
@@ -125,6 +126,22 @@ export default function Profile() {
       setShowListingsError(true)
     }
   }
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
  
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -230,7 +247,11 @@ export default function Profile() {
                 </p>
               </Link>
               <div className='flex flex-col items-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button
+                  onClick={()=>handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'>
+                  Delete
+                </button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
