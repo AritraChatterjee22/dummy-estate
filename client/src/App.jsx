b0242ec0caf5c5d4b278f2b612ced7ff43bd34b7
Aritra <chatterjee.aritra@protonmail.com>
2023-10-28 02:06:10 +0530

    fixing app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
-import SignnUp from './pages/SignUp'
+import SignUp from './pages/SignUp'
 import Profile from "./pages/Profile"
 import About from './pages/About'
 import Header from './components/Header'
@@ -19,7 +19,7 @@ export default function App() {
       <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/sign-in' element={<SignIn/>}/>
-      <Route path='/sign-up' element={<SignnUp/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/search' element={<Search/>}/>
       <Route path='/listing/:listingId' element={<Listing/>}/>
